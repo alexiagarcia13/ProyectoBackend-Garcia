4d324e55090da81db6dc05d2b49d1dadecf565ce
alexiagarcia13 <alexia4rs013@gmail.com>
2023-08-15 18:16:43 -0300

    ¨agrupamos¨

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function mostrarLista(arreglo) {
-    if(Array.isArray(arreglo)) {
-    if (!arreglo.length) {
-        console.log('Lista vacia')
-    }else{
-        arreglo.forEach((elem)=>console.log(elem))
-        console.log('la longitud es de: ${arreglo.length}')
-    }
+  if(Array.isArray(arreglo)) {
+  if (!arreglo.length) {
+      console.log('Lista vacia')
+  }else{
+      arreglo.forEach((elem)=>console.log(elem))
+      console.log('la longitud es de: ${arreglo.length}')
+  }
 }else{
-        console.log('No es un arreglo');
-    }}
+      console.log('No es un arreglo');
+  }}
 
 mostrarLista([5,4])
 
@@ -16,61 +16,56 @@ mostrarLista([5,4])
 
 
 class Product {
-    constructor(code, name, price, description, thumbnail, stock) {
-      this.code = code;
-      this.name = name;
-      this.price = price;
-      this.description = description;
-      this.thumbnail = thumbnail;
-      this.stock = stock
-    }
+  constructor(code, name, price, description, thumbnail, stock) {
+    this.code = code;
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.thumbnail = thumbnail;
+    this.stock = stock
   }
-  
-  class ProductManager {
-    constructor() {
-      this.products = [];
-      this.nextId = 1;
+}
+
+
+
+  addProduct(code, name, price, description, thumbnail, stock) ;{
+    if (!code || !name || !price ||!description ||!thumbnail ||!stock ) {
+      console.log("Error: Todos los campos son obligatorios.");
+      return;
     }
-  
-    addProduct(code, name, price, description, thumbnail, stock) {
-      // Validar que todos los campos sean obligatorios
-      if (!code || !name || !price ||!description ||!thumbnail ||!stock ) {
-        console.log("Error: Todos los campos son obligatorios.");
+
+    
+    for (const product of this.products) {
+      if (product.code === code) {
+        console.log(`Error: El código '${code}' ya existe.`);
         return;
       }
-  
-      
-      for (const product of this.products) {
-        if (product.code === code) {
-          console.log(`Error: El código '${code}' ya existe.`);
-          return;
-        }
-      }
-  
-      
-      const product = new Product(code, name, price, description, thumbnail, stock );
-      product.id = this.nextId;
-      this.nextId++;
-  
-     
-      this.products.push(product);
     }
-  
-    getProducts() {
-      return this.products;
-    }
-  
-    getProductById(id) {
-      for (const product of this.products) {
-        if (product.id === id) {
-          return product;
-        }
-      }
-  
-      
-      console.log("Error: Producto no encontrado.");
-    }
+
+    
+    const product = new Product(code, name, price, description, thumbnail, stock );
+    product.id = this.nextId;
+    this.nextId++;
+
+   
+    this.products.push(product);
   }
+
+  getProducts(); {
+    return this.products;
+  }
+
+  getProductById(id); {
+    for (const product of this.products) {
+      if (product.id === id) {
+        return product;
+      }
+    }
+
+    
+    console.log("Error: Producto no encontrado.");
+  }
+
 
 const manager = new ProductManager();
 
@@ -82,12 +77,12 @@ manager.addProduct("P003", "Texanas Serena", 6990,'texana cana media', img/venta
 // Obtener todos los productos
 const products = manager.getProducts();
 for (const product of products) {
-  console.log(`ID: ${product.id}, Código: ${product.code}, Nombre: ${product.name}, Precio: ${product.price}, Descripcion: ${product.description}, Imagen: ${product.thumbnail}, Stock: ${product.stock}`);
+console.log(`ID: ${product.id}, Código: ${product.code}, Nombre: ${product.name}, Precio: ${product.price}, Descripcion: ${product.description}, Imagen: ${product.thumbnail}, Stock: ${product.stock}`);
 }
 
 // Buscar un producto por ID
 const foundProduct = manager.getProductById(2);
 if (foundProduct) {
-  console.log(`Producto encontrado: ${foundProduct.name}`);
+console.log(`Producto encontrado: ${foundProduct.name}`);
 }
   
--- src/ProductManager.js
+++ src/ProductManager.js
@@ -19,3 +19,4 @@ class ProductManager {
 }
 
 module.exports = ProductManager;
+  
